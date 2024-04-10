f7d2411617aa52cc59fe0d631b542cc39c24187c
Maksim Stogniy <mstogniy@ya.ru>
2024-04-11 00:10:10 +0300

    Initial Commit

---
+++ MyCard/MyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCardApp.swift
+//  MyCard
+//
+//  Created by  Maksim Stogniy on 11.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
